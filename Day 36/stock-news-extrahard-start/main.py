d2400049cb7a0b40f44d860bf241db64eb27ef85
Jean-Baptiste PINET <jbp.computing@gmail.com>
2021-02-13 22:55:48 +0100

    Changes in Day 36 files

--- Day 36/stock-news-extrahard-start/main.py
+++ Day 36/stock-news-extrahard-start/main.py
@@ -1,13 +1,17 @@
 import requests
-from datetime import datetime, timedelta
 from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-AV_API_KEY = "7FNJH3IBZR2RTK2L"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+STOCK_API_KEY = "7FNJH3IBZR2RTK2L"
+
+NEWS_ENDPOINT = "https://newsapi.org/v2/top-headlines"
 NEWS_API_KEY = "2128f6ca7f294e06a2c82cd49aca1634"
+
 TWILIO_ACCOUNT_SID = "ACeb1b8f647238fb1313e69c2ebdd37f50"
-TWILIO_AUTH_TOKEN = "38cb71543b3e3cc13097651d18fb82b5"
+TWILIO_AUTH_TOKEN = "0d2d81fa63b8dbcc8a58b9c725e359aa"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -15,24 +19,20 @@ parameters = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
     "outputsize": "compact",
-    "apikey": AV_API_KEY
+    "apikey": STOCK_API_KEY
 }
 
-response = requests.get(url="https://www.alphavantage.co/query", params=parameters)
+response = requests.get(url=STOCK_ENDPOINT, params=parameters)
 response.raise_for_status()
 
-today = datetime.now().date()
-yesterday = str(today - timedelta(days=1))
-day_before_yesterday = str(today - timedelta(days=2))
-
-close_yesterday = response.json()["Time Series (Daily)"][yesterday]["4. close"]
-close_day_before_yesterday = response.json()["Time Series (Daily)"][day_before_yesterday]["4. close"]
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+close_yesterday = data_list[0]["4. close"]
+close_day_before_yesterday = data_list[1]["4. close"]
 
 diff = round((float(close_yesterday) / float(close_day_before_yesterday) - 1) * 100, 1)
 
-if diff >= 5 or diff <= -5:
-    print("Get news")
-
+if abs(diff) >= 0:
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
     parameters = {
@@ -40,50 +40,32 @@ if diff >= 5 or diff <= -5:
         "apiKey": NEWS_API_KEY
     }
 
-    response = requests.get(url="https://newsapi.org/v2/top-headlines", params=parameters)
+    response = requests.get(url=NEWS_ENDPOINT, params=parameters)
     response.raise_for_status()
 
-    data = response.json()["articles"]
-
-    news = []
-    if len(data) > 3:
-        news_number = 3
-    else:
-        news_number = len(data)
-
-    for i in range(news_number):
-        news.append(data[i])
-
-    print(news)
+    news = response.json()["articles"][:3]
 
 ## STEP 3: Use https://www.twilio.com
 # Send a separate message with the percentage change and each article's title and description to your phone number.
 
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-    percentage_message = f"{STOCK}: "
+    percentage_message = ""
 
     if diff > 0:
         percentage_message += f"🔺{diff}%"
     else:
         percentage_message += f"🔻{diff}%"
 
-    message = client.messages \
-                .create(
-                    body=percentage_message,
-                    from_="+15863718448",
-                    to="+33665708618"
-                )
+    articles = [f"{STOCK}:{percentage_message}\nHeadline: {new['title']}\nBrief: {new['description']}" for new in news]
 
-    for new in news:
-        new_message = f"Headline: {new['title']}\nBrief: {new['description']}"
-
+    for article in articles:
         message = client.messages \
-            .create(
-            body=new_message,
-            from_="+15863718448",
-            to="+33665708618"
-        )
+                    .create(
+                        body=article,
+                        from_="+15863718448",
+                        to="+33665708618"
+                    )
 
 # Optional: Format the SMS message like this:
 """
